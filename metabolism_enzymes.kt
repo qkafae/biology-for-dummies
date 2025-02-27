aac826aede86c256de0ddba1f4282984207e8e61
kafae <104088216+qkafae@users.noreply.github.com>
2025-02-27 23:32:22 +0800

    Update metabolism_enzymes.kt

--- metabolism_enzymes.kt
+++ metabolism_enzymes.kt
@@ -22,6 +22,18 @@ private abstract class Metabolism {
     abstract val energyChange: EnergyChange
 }
 
+private abstract class Protein {
+    abstract val name: String
+}
+
+private abstract class Molecule {
+    abstract val name: String
+}
+
+private class Substrate: Molecule() {
+    override val name = "substrate"
+}
+
 private class Anabolism: Metabolism() {
     override val reaction = Reaction.BUILDING_UP
     override val energyChange = EnergyChange.ABSORB
@@ -32,14 +44,37 @@ private class Catabolism: Metabolism() {
     override val energyChange = EnergyChange.RELEASE
 }
 
-private class Enzymes {
+private class Enzyme: Protein() {
     // enzymes are biological catalysts, providing alternative pathway with lower activation energy
+    
+    override val name = "enzyme"
     
     val isUsedUpByReaction = false
     val isChemicallyAlteredByReaction = false
     val isReusable = true
     val amountNeededPerReaction = Amount.SMALL
-    val baseType = EnzymeStatus.ENZYME
+    
+    fun start() {
+        val activeSites: Array<Substrate?> = arrayOf(null, null)
+        
+        var type = EnzymeStatus.ENZYME
+        
+        Thread.sleep(1000)
+        
+        for (i in activeSites.indices) {
+            activeSites[i] = Substrate()
+        }
+        
+        type = EnzymeStatus.ENZYME_SUBSTRATE
+        
+        Thread.sleep(1000)
+        
+        for (i in activeSites.indices) {
+            activeSites[i] = null
+        } 
+        
+        type = EnzymeStatus.ENZYME
+    }
 }
 
 private fun main() {
